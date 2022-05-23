fea47425a03ca5bed3bb27efc41f6b5bcaa957bd
miguelleonmarti <miguelleonmarti@gmail.com>
2022-05-23 19:21:11 +0100

    feat: contract developed and tested

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -2,6 +2,122 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract CoinFlip {
-  constructor() public {
-  }
+    // owner
+    address private owner;
+    uint256 public balance = 0;
+
+    // variables
+    uint256 public betPrice = 200000000000000000;
+    uint256 private randNonce = 0;
+
+    // stats
+    uint256 public gamesCount = 0;
+    uint256 public headsWins = 0;
+    uint256 public tailsWins = 0;
+
+    // virtual queues
+    mapping(uint256 => address) private headsQueue;
+    mapping(uint256 => address) private tailsQueue;
+    uint256 public firstHead = 1;
+    uint256 public lastHead = 0;
+    uint256 public firstTail = 1;
+    uint256 public lastTail = 0;
+
+    event NeedToWait(address player);
+    event Result(address winner, address loser, uint256 result);
+    event BetPriceUpdate(uint256 betPrice);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this.");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function play(uint256 coinOption) public payable {
+        require(msg.value == betPrice, "Invalid price");
+        if (coinOption == 0) {
+            // eligió heads
+            if (lastTail < firstTail) {
+                enqueue(msg.sender, 0);
+                balance += msg.value;
+            } else {
+                address player = dequeue(1);
+                uint256 result = flipCoin();
+                if (result == 0) {
+                    // gana heads
+                    headsWins += 1;
+                    gamesCount += 1;
+                    emit Result(msg.sender, player, result);
+                    payable(msg.sender).transfer(400000000000000000);
+                } else {
+                    // gana tails
+                    tailsWins += 1;
+                    gamesCount += 1;
+                    emit Result(player, msg.sender, result);
+                    payable(player).transfer(400000000000000000);
+                }
+            }
+        } else if (coinOption == 1) {
+            // eligió tails
+            if (lastHead < firstHead) {
+                enqueue(msg.sender, 1);
+                balance += msg.value;
+            } else {
+                address player = dequeue(0);
+                uint256 result = flipCoin();
+                if (result == 0) {
+                    // gana heads
+                    headsWins += 1;
+                    gamesCount += 1;
+                    emit Result(player, msg.sender, result);
+                    payable(player).transfer(400000000000000000);
+                } else {
+                    // gana tails
+                    tailsWins += 1;
+                    gamesCount += 1;
+                    emit Result(msg.sender, player, result);
+                    payable(msg.sender).transfer(400000000000000000);
+                }
+            }
+        }
+    }
+
+    function enqueue(address player, uint256 queue) private {
+        if (queue == 0) {
+            lastHead += 1;
+            headsQueue[lastHead] = player;
+        } else if (queue == 1) {
+            lastTail += 1;
+            headsQueue[lastTail] = player;
+        }
+    }
+
+    function dequeue(uint256 queue) private returns (address player) {
+        if (queue == 0) {
+            require(lastHead >= firstHead, "Head queue is empty");
+            player = headsQueue[firstHead];
+            delete headsQueue[firstHead];
+            firstHead += 1;
+        } else if (queue == 1) {
+            require(lastTail >= firstTail, "Tail queue is Empty");
+            player = tailsQueue[firstTail];
+            delete tailsQueue[firstTail];
+            firstTail += 1;
+        }
+    }
+
+    function flipCoin() private returns (uint256 result) {
+        randNonce++;
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % 2;
+    }
+
+    function setBetPrice() public view onlyOwner returns (bool success) {}
 }
--- test/coin_flip.js
+++ test/coin_flip.js
@@ -5,9 +5,68 @@ const CoinFlip = artifacts.require("CoinFlip");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("CoinFlip", function (/* accounts */) {
-  it("should assert true", async function () {
-    await CoinFlip.deployed();
-    return assert.isTrue(true);
+contract("CoinFlip", function (accounts) {
+  it("shoud initialize the variables", async function () {
+    const instance = await CoinFlip.deployed();
+
+    const betPrice = await instance.betPrice.call();
+    assert.equal(BigInt(betPrice), 200000000000000000);
+
+    const balance = BigInt(await instance.balance.call());
+    assert.equal(balance, 0);
+
+    const gamesCount = BigInt(await instance.gamesCount.call());
+    assert.equal(gamesCount, 0);
+
+    const headsWins = BigInt(await instance.headsWins.call());
+    assert.equal(headsWins, 0);
+
+    const tailsWins = BigInt(await instance.tailsWins.call());
+    assert.equal(tailsWins, 0);
+  });
+
+  it("should enqueue the first player", async function () {
+    const instance = await CoinFlip.deployed();
+
+    let balance = BigInt(await instance.balance.call());
+    assert.equal(balance, 0);
+
+    await instance.play(0, { from: accounts[0], value: 200000000000000000 });
+    balance = BigInt(await instance.balance.call());
+    assert.equal(balance, 200000000000000000);
+  });
+
+  it("should flip a coin and send the prize to the winner", async function () {
+    const instance = await CoinFlip.deployed();
+
+    // const firstBalanceBefore = BigInt(await web3.eth.getBalance(accounts[0]));
+    // const secondBalanceBefore = BigInt(await web3.eth.getBalance(accounts[1]));
+
+    const results = await instance.play(1, { from: accounts[1], value: 200000000000000000 });
+
+    assert.equal(results.logs.length, 1);
+    assert.equal(results.logs[0].event, "Result");
+    const { winner, loser, result } = results.logs[0].args;
+
+    const headsWins = await instance.headsWins.call();
+    const tailsWins = await instance.tailsWins.call();
+
+    if (result.toNumber() === 0) {
+      assert.equal(headsWins.toNumber(), 1);
+      assert.equal(tailsWins.toNumber(), 0);
+      assert.equal(winner, accounts[0]);
+      assert.equal(loser, accounts[1]);
+    } else {
+      assert.equal(tailsWins.toNumber(), 1);
+      assert.equal(headsWins.toNumber(), 0);
+      assert.equal(loser, accounts[0]);
+      assert.equal(winner, accounts[1]);
+    }
+
+    const gamesCount = BigInt(await instance.gamesCount.call());
+    assert.equal(gamesCount, 1);
+
+    // const firstBalanceAfter = BigInt(await web3.eth.getBalance(accounts[0]));
+    // const secondBalanceAfter = BigInt(await web3.eth.getBalance(accounts[1]));
   });
 });
