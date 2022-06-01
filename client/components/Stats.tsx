e264af94b63ad95f5d9a97fdd18b7d055389fbf1
miguelleonmarti <miguelleonmarti@gmail.com>
2022-06-01 13:03:58 +0100

    styles: stats ui

--- client/components/Stats.tsx
+++ client/components/Stats.tsx
@@ -2,11 +2,18 @@ import type { NextPage } from "next";
 import styles from "../styles/Stats.module.scss";
 
 const Stats: NextPage<{ stats: { gamesCount: number; headsWins: number; tailsWins: number } }> = ({ stats }) => {
+  function getPercentage(wins: number) {
+    return ((wins * 100) / stats.gamesCount).toFixed(1);
+  }
   return (
     <div className={styles.stats}>
+      <div>
+        H: {stats.headsWins} ({getPercentage(stats.headsWins)}%)
+      </div>
       <div>Games: {stats.gamesCount}</div>
-      <div>Heads wins: {stats.headsWins}</div>
-      <div>Tails wins: {stats.tailsWins}</div>
+      <div>
+        T: {stats.tailsWins} ({getPercentage(stats.tailsWins)}%)
+      </div>
     </div>
   );
 };
